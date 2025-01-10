1ece6d98c93c457d1a6854756bd248c51d2486c6
Aymane D. Hlamnach <dara-@outlook.jp>
2025-01-10 23:02:00 +0100

    fix: fixed the release for real this time

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import youcan from '@youcan/lint';
 
 export default youcan(
   {
-    ignores: ['node_modules', 'dist'],
+    ignores: ['node_modules', 'dist', 'packages/celeste-icons/assets'],
     type: 'lib',
     formatters: true,
     vue: true,
