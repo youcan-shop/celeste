ad2126f9177f54cca7eb16edb366d90a125557f4
Aymane Dara Hlamnach <dara-@outlook.jp>
2025-04-07 10:54:21 +0100

    fix: exports (#22)

---
+++ packages/celeste-vue/src/components/dropdown/index.ts
@@ -0,0 +1,5 @@
+export * from './dropdown-item.vue';
+export { default as DropdownItem } from './dropdown-item.vue';
+
+export * from './dropdown.vue';
+export { default as Dropdown } from './dropdown.vue';
