95eedafd261bad7b7fa676fef36cec1692a4cb79
Eihab Khan <mastereihab@gmail.com>
2025-05-26 10:23:44 +0100

    chore: add delegatedProps composable

---
+++ packages/celeste-vue/src/composables/delegatedProps.ts
@@ -0,0 +1,12 @@
+// composables/useDelegatedProps.ts
+import { computed, type ComputedRef } from 'vue';
+
+export function useDelegatedProps(props: any, excludeKeys: string | string[] = ['class']): ComputedRef<any> {
+  return computed(() => {
+    const keysToExclude = Array.isArray(excludeKeys) ? excludeKeys : [excludeKeys];
+    const delegated = { ...props };
+    keysToExclude.forEach(key => delete delegated[key]);
+
+    return delegated;
+  });
+}
