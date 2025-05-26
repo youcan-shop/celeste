cbf2b7a7c4fd94078ee34dec5eda67f8de101665
Eihab Khan <mastereihab@gmail.com>
2025-05-26 14:45:04 +0100

    fix: tighten typing for composable

--- packages/celeste-vue/src/composables/use-delegated-props.ts
+++ packages/celeste-vue/src/composables/use-delegated-props.ts
@@ -1,11 +1,20 @@
 import { computed, type ComputedRef } from 'vue';
 
-export function useDelegatedProps(props: any, excludeKeys: string | string[] = ['class']): ComputedRef<any> {
+export function useDelegatedProps<
+  T extends Record<string, any>,
+  K extends keyof T,
+>(
+  props: T,
+  excludeKeys: K | K[] = ['class'] as K[],
+): ComputedRef<Omit<T, K>> {
   return computed(() => {
     const keysToExclude = Array.isArray(excludeKeys) ? excludeKeys : [excludeKeys];
-    const delegated = { ...props };
-    keysToExclude.forEach(key => delete delegated[key]);
+    const delegated = { ...props } as T;
 
-    return delegated;
+    keysToExclude.forEach((key) => {
+      delete delegated[key];
+    });
+
+    return delegated as Omit<T, K>;
   });
 }
