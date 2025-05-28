3cdfa5c0b1f2e7447b4da6d8241b94f4b72efa7c
Badreddine Ibril <badreddine.ibril@youcan.shop>
2025-05-28 16:07:04 +0100

    feat: slider

---
+++ packages/celeste-vue/src/components/slider/stories/slider.stories.ts
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+import { ref } from 'vue';
+import Slider from '../slider.vue';
+
+const meta: Meta<typeof Slider> = {
+  title: 'Components/Slider',
+  component: Slider,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Slider>;
+
+export const Default: Story = {
+  args: {
+    max: 100,
+    step: 1,
+  },
+
+  render: args => ({
+    components: { Slider },
+    setup() {
+      const value = ref([50]);
+      return { args, value };
+    },
+    template: `
+      <Slider v-model="value" v-bind="args" />
+    `,
+  }),
+};
