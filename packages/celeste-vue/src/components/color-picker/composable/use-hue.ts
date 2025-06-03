b0cb5db7335f33700758f33c288b821ded04c2d6
Eihab Khan <mastereihab@gmail.com>
2025-06-03 16:36:43 +0100

    fix: fix hue jittering when dragging color area thumb

--- packages/celeste-vue/src/components/color-picker/composable/use-hue.ts
+++ packages/celeste-vue/src/components/color-picker/composable/use-hue.ts
@@ -1,7 +1,5 @@
 import tinycolor from 'tinycolor2';
-import { ref, type Ref, watch, type WritableComputedRef } from 'vue';
-
-const hueSourceMap = new WeakMap<tinycolor.Instance, boolean>();
+import { ref, type Ref, type WritableComputedRef } from 'vue';
 
 interface UseHueRefReturn {
   hueRef: Ref<number>;
@@ -11,27 +9,14 @@ interface UseHueRefReturn {
 export function useHueRef(tinyColorRef: WritableComputedRef<tinycolor.Instance, tinycolor.Instance>): UseHueRefReturn {
   const hueRef = ref(0);
 
-  watch(tinyColorRef, (tinyColorInstance) => {
-    if (hueSourceMap.has(tinyColorInstance)) {
-      return;
-    }
-
-    const newHue = tinyColorInstance.toHsl().h;
-    if (newHue === 0 && hueRef.value !== 0) {
-      return;
-    }
-    hueRef.value = newHue;
-  }, { immediate: true });
+  hueRef.value = tinyColorRef.value.toHsl().h;
 
   const updateHueRef = (newHue: number): any => {
     const newColorInstance = tinycolor({
       ...tinyColorRef.value.toHsl(),
       h: newHue,
     });
-
-    hueSourceMap.set(newColorInstance, true);
     tinyColorRef.value = newColorInstance;
-
     hueRef.value = newHue;
   };
 
