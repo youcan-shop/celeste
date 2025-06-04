c28cb4154f57182d9bb8c7bc00a65392b0106dfe
Anas <93322743+bj-anas@users.noreply.github.com>
2025-06-04 14:32:39 +0100

    feat: add Rich Editor

---
+++ packages/celeste-vue/src/components/rich-editor/stories/rich-editor.stories.ts
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+import { ref } from 'vue';
+import RichEditor from '../rich-editor.vue';
+
+const meta: Meta<typeof RichEditor> = {
+  title: 'Components/RichEditor',
+  component: RichEditor,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof RichEditor>;
+
+const content = ref('<p>Hello Tiptap 👋</p>');
+
+export const Default: Story = {
+  args: {
+    modelValue: content.value,
+    maxLimit: 200,
+  },
+
+  render: args => ({
+    components: { RichEditor },
+    setup() {
+      return { args };
+    },
+    template: `
+      <RichEditor v-bind="args" />
+    `,
+  }),
+};
