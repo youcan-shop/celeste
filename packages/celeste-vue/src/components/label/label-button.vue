9600188b1eaad97f5d0426de892dbcb6acf518b6
Badreddine Ibril <badreddine.ibril@youcan.shop>
2025-06-04 11:34:10 +0100

    fix: extends base isssue

--- packages/celeste-vue/src/components/label/label-button.vue
+++ packages/celeste-vue/src/components/label/label-button.vue
@@ -1,29 +1,16 @@
 <script setup lang="ts">
-import type { PrimitiveProps } from 'radix-vue';
-import type { HTMLAttributes } from 'vue';
 import LinkButton, { type LinkButtonProps } from '@/components/button/link-button.vue';
 import { useDelegatedProps } from '@/composables/use-delegated-props';
 import clsx from 'clsx';
 
-const props = withDefaults(defineProps<LabelButtonProps>(), {
-  type: 'gray',
-});
+const props = defineProps</* @vue-ignore */ LinkButtonProps>();
 const delegatedProps = useDelegatedProps(props, 'class');
-</script>
-
-<script lang="ts">
-interface LabelButtonProps extends PrimitiveProps {
-  class?: HTMLAttributes['class'];
-  size?: LinkButtonProps['size'];
-  type?: LinkButtonProps['type'];
-  underline?: LinkButtonProps['underline'];
-  disabled?: LinkButtonProps['disabled'];
-}
 </script>
 
 <template>
   <LinkButton
     v-bind="delegatedProps"
+    :type="props.type || 'gray'"
     :class="clsx('celeste-label-button', props.class)"
   >
     <slot />
