5615ff663e865beece1f3741a9ebe4e92aeef7ca
Eihab Khan <143792300+eihabkhan@users.noreply.github.com>
2025-06-05 14:35:25 +0100

    chore: Upgrade storybook to v9 (#43)

--- packages/celeste-vue/src/components/label/stories/label.stories.ts
+++ packages/celeste-vue/src/components/label/stories/label.stories.ts
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/vue3';
+import type { Meta, StoryObj } from '@storybook/vue3-vite';
 import Label from '../label.vue';
 
 const meta: Meta<typeof Label> = {
