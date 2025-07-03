8d7f6a9433935ed463f6ea3b739788fb93178cfc
Eihab Khan <mastereihab@gmail.com>
2025-07-03 14:15:28 +0100

    chore: add picker components exports

--- packages/celeste-vue/src/components/color-picker/index.ts
+++ packages/celeste-vue/src/components/color-picker/index.ts
@@ -1,2 +1,14 @@
+export * from './alpha-slider.vue';
+export { default as AlphaSlider } from './alpha-slider.vue';
+
+export * from './color-area.vue';
+export { default as ColorArea } from './color-area.vue';
+
 export * from './color-picker.vue';
 export { default as ColorPicker } from './color-picker.vue';
+
+export * from './color-swatch.vue';
+export { default as ColorSwatch } from './color-swatch.vue';
+
+export * from './hue-slider.vue';
+export { default as HueSlider } from './hue-slider.vue';
