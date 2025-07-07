f9de9c9e8e1b96fa6ce2f09482c8facbebac70d9
Eihab Khan <mastereihab@gmail.com>
2025-07-07 10:15:00 +0100

    chore: improve utils typing

--- packages/celeste-vue/src/components/color-picker/utils.ts
+++ packages/celeste-vue/src/components/color-picker/utils.ts
@@ -1,3 +1,9 @@
+// TYPES
+export interface Coordinates {
+  x: number;
+  y: number;
+}
+
 // VALUES
 export const DEFAULT_SWATCH = [
   '#717784ff',
@@ -13,9 +19,11 @@ export const DEFAULT_SWATCH = [
 // MATH
 export function getFractionDigit(data: number | string): number {
   const str = data.toString();
+
   if (str.includes('.')) {
     return str.split('.')[1].length;
   }
+
   return 0;
 }
 
@@ -28,34 +36,34 @@ export function truncColorValue(value: string): number {
 }
 
 // DOM
-// TODO: Return type
-export function getPageXYFromEvent(e: MouseEvent | TouchEvent): any {
-  // including scroll offset
-  const res: { x: number; y: number } = { x: 0, y: 0 };
+export function getPageXYFromEvent(e: MouseEvent | TouchEvent): Coordinates {
+  const coordinates: Coordinates = { x: 0, y: 0 };
   if (e instanceof MouseEvent) {
-    res.x = e.pageX;
-    res.y = e.pageY;
+    coordinates.x = e.pageX;
+    coordinates.y = e.pageY;
   }
+
   if (typeof TouchEvent !== 'undefined' && e instanceof TouchEvent) {
-    res.x = (e.touches ? e.touches[0].pageX : e.changedTouches ? e.changedTouches[0].pageX : 0);
-    res.y = (e.touches ? e.touches[0].pageY : e.changedTouches ? e.changedTouches[0].pageY : 0);
+    coordinates.x = (e.touches ? e.touches[0].pageX : e.changedTouches ? e.changedTouches[0].pageX : 0);
+    coordinates.y = (e.touches ? e.touches[0].pageY : e.changedTouches ? e.changedTouches[0].pageY : 0);
   }
-  return res;
+
+  return coordinates;
 }
 
-// TODO: Return type
-export function getScrollXY(): any {
+export function getScrollXY(): Coordinates {
   const x = window.scrollX || window.pageXOffset || document.documentElement.scrollLeft || 0;
   const y = window.scrollY || window.pageYOffset || document.documentElement.scrollTop || 0;
+
   return { x, y };
 }
 
 /** get the position of the container relative to the document’s edge, regardless of any scrolling that has occurred */
-// TODO: Return type
-export function getAbsolutePosition(container: HTMLElement): any {
+export function getAbsolutePosition(container: HTMLElement): Coordinates {
   const { x: scrollX, y: scrollY } = getScrollXY();
 
   const rect = container.getBoundingClientRect();
+
   return {
     x: rect.left + scrollX,
     y: rect.top + scrollY,
