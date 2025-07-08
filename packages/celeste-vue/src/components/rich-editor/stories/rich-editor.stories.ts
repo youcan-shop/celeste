d25f89534dd1c365ddd7110749b61eba2e5893fd
Anas <93322743+bj-anas@users.noreply.github.com>
2025-07-08 14:49:45 +0100

    feat: add placeholder

--- packages/celeste-vue/src/components/rich-editor/stories/rich-editor.stories.ts
+++ packages/celeste-vue/src/components/rich-editor/stories/rich-editor.stories.ts
@@ -4,6 +4,36 @@ import RichEditor from '../rich-editor.vue';
 const meta: Meta<typeof RichEditor> = {
   title: 'Components/RichEditor',
   component: RichEditor,
+
+  argTypes: {
+    customToolbar: {
+      description: `An array of toolbar item slugs that defines the order and visibility of editor actions.
+        * item_slugs: [
+          'heading',
+          'fontSize',
+          'color',
+          'bold',
+          'italic',
+          'underline',
+          'strike',
+          'bulletList',
+          'orderedList',
+          'align',
+          'link',
+          'undo',
+          'redo',
+          'clear',
+        ]
+
+        * 'divider': use it for separation.
+
+        Example: ['bold', 'divider', 'color']
+      `,
+    },
+    showExtraSettings: {
+      description: 'Controls the visibility of the dropdown menu containing options like copy, cut, delete, and duplicate.',
+    },
+  },
 };
 
 export default meta;
@@ -13,6 +43,7 @@ type Story = StoryObj<typeof RichEditor>;
 export const Default: Story = {
   args: {
     modelValue: '',
+    customToolbar: [],
   },
 
   render: args => ({
@@ -28,7 +59,8 @@ export const Default: Story = {
 
 export const CustomToolbar: Story = {
   args: {
-    modelValue: 'Hello world 👋',
+    modelValue: '',
+    placeholder: 'Write something...',
     customToolbar: [
       'heading',
       'divider',
