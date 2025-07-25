fed2ac27c0f4e1fc4aae96d0164d147a48dc8700
Aymane Dara Hlamnach <dara-@outlook.jp>
2025-07-25 12:12:39 +0100

    feat: sonner (#56)

---
+++ packages/celeste-vue/src/utils/sonner.ts
@@ -0,0 +1,72 @@
+import type { SonnerAlertProps } from '@/components/sonner/sonner-alert.vue';
+import SonnerAlert from '@/components/sonner/sonner-alert.vue';
+import { markRaw } from 'vue';
+import { type ExternalToast, toast as sonner } from 'vue-sonner';
+
+export interface ToastOptions extends ExternalToast {
+  state?: SonnerAlertProps['state'];
+  variant?: SonnerAlertProps['variant'];
+  size?: SonnerAlertProps['size'];
+  dismissable?: SonnerAlertProps['dismissable'];
+}
+
+function createToast(title: string, data?: ToastOptions): string | number {
+  const {
+    state = 'success',
+    variant = 'stroke',
+    size = 'lg',
+    dismissable = true,
+    ...externalToastProps
+  } = data || {};
+
+  let toastId: string | number;
+
+  const dismissToast = (): void => {
+    sonner.dismiss(toastId);
+  };
+
+  toastId = sonner.custom(markRaw(SonnerAlert), {
+    ...externalToastProps,
+    componentProps: {
+      title,
+      description: data?.description,
+      state,
+      variant,
+      size,
+      dismissable,
+      onDismiss: dismissToast,
+    },
+  });
+
+  return toastId;
+}
+
+export const toast = Object.assign(createToast, {
+  success: (title: string, data?: Omit<ToastOptions, 'state'>): string | number => {
+    return createToast(title, { ...data, state: 'success' });
+  },
+
+  error: (title: string, data?: Omit<ToastOptions, 'state'>): string | number => {
+    return createToast(title, { ...data, state: 'error' });
+  },
+
+  warning: (title: string, data?: Omit<ToastOptions, 'state'>): string | number => {
+    return createToast(title, { ...data, state: 'warning' });
+  },
+
+  info: (title: string, data?: Omit<ToastOptions, 'state'>): string | number => {
+    return createToast(title, { ...data, state: 'information' });
+  },
+
+  feature: (title: string, data?: Omit<ToastOptions, 'state'>): string | number => {
+    return createToast(title, { ...data, state: 'feature' });
+  },
+
+  dismiss: (id?: string | number): void => {
+    sonner.dismiss(id);
+  },
+
+  loading: (title: string, data?: ExternalToast): string | number => {
+    return sonner.loading(title, data);
+  },
+});
