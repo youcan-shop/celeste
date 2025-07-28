02fe4dc94eebc15c8a3e17f58ca6358cc29074c4
Fatima Ezzahra Elmenoun <130572576+fatima-zz@users.noreply.github.com>
2025-07-28 15:22:47 +0100

    feat: progress bar (#45)
    Co-authored-by: Eihab Khan <143792300+eihabkhan@users.noreply.github.com>

--- packages/celeste-vue/src/components/progress/index.ts
+++ packages/celeste-vue/src/components/progress/index.ts
@@ -1,2 +1,5 @@
+export * from './progress-bar.vue';
+export { default as ProgressBar } from './progress-bar.vue';
+
 export * from './progress-circle.vue';
 export { default as ProgressCircle } from './progress-circle.vue';
