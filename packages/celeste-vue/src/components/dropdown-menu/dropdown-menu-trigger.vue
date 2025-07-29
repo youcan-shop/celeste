6a0a95b3e50718c5c607247498ec78717a43b6fa
Eihab Khan <mastereihab@gmail.com>
2025-07-29 15:29:22 +0100

    chore: dropdown menu trigger cleanup

--- packages/celeste-vue/src/components/dropdown-menu/dropdown-menu-trigger.vue
+++ packages/celeste-vue/src/components/dropdown-menu/dropdown-menu-trigger.vue
@@ -7,7 +7,12 @@ const forwardedProps = useForwardProps(props);
 </script>
 
 <template>
-  <DropdownMenuTrigger class="celeste-dropdown-menu-trigger" v-bind="forwardedProps">
+  <DropdownMenuTrigger
+
+    class="celeste-dropdown-menu-trigger"
+    v-bind="forwardedProps"
+    as="div"
+  >
     <slot />
   </DropdownMenuTrigger>
 </template>
@@ -16,28 +21,9 @@ const forwardedProps = useForwardProps(props);
 .celeste-dropdown-menu-trigger {
   display: flex;
   align-items: center;
-  padding: var(--spacing-8);
-  border: 1px solid var(--color-stroke-soft-200);
   border-radius: var(--radius-8);
-  background-color: var(--color-bg-white-0);
-  box-shadow: var(--shadow-regular-xs);
-  color: var(--color-text-sub-600);
   cursor: pointer;
   gap: var(--spacing-8);
-
-  :deep(i) {
-    color: var(--color-icon-soft-400);
-  }
-
-  &:hover {
-    background: var(--color-bg-weak-50);
-  }
-
-  &[data-state='open'] {
-    border: 1px solid var(--color-stroke-strong-950);
-    box-shadow: var(--shadow-buttons-important-focus);
-    color: var(--color-text-strong-950);
-  }
 
   &[data-disabled] {
     background: var(--color-bg-weak-50);
