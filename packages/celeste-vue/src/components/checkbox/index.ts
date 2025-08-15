8cd598610a023420816e7339f674c6941e590bd1
Aymane Dara Hlamnach <dara-@outlook.jp>
2025-08-15 11:51:43 +0100

    fix: export checkbox-card

--- packages/celeste-vue/src/components/checkbox/index.ts
+++ packages/celeste-vue/src/components/checkbox/index.ts
@@ -1,3 +1,6 @@
+export * from './checkbox-card.vue';
+export { default as CheckboxCard } from './checkbox-card.vue';
+
 export * from './checkbox-label.vue';
 export { default as CheckboxLabel } from './checkbox-label.vue';
 
