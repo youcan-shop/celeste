ae3b37f3e43b6d2a8683a8ca3a3ae538b28b0777
Badreddine Ibril <badreddine.ibril@youcan.shop>
2025-08-22 10:25:32 +0100

    chore: add component stories

--- packages/celeste-vue/src/components/counter-input/stories/counter-input.stories.ts
+++ packages/celeste-vue/src/components/counter-input/stories/counter-input.stories.ts
@@ -12,8 +12,9 @@ type Story = StoryObj<typeof CounterInput>;
 
 export const Default: Story = {
   args: {
-    defaultValue: 0,
-    max: 10,
+    defaultValue: 16,
+    max: 20,
+    min: 10,
   },
 
   render: args => ({
@@ -22,7 +23,65 @@ export const Default: Story = {
       return { args };
     },
     template: `
-      <CounterInput />
+      <CounterInput v-bind="args" />
+    `,
+  }),
+};
+
+export const Size: Story = {
+  args: {
+    defaultValue: 16,
+    max: 20,
+    min: 10,
+  },
+
+  render: args => ({
+    components: { CounterInput },
+    setup() {
+      return { args };
+    },
+    template: `
+      <div style="display: grid; gap: 12px;">
+        <CounterInput v-bind="args" size="md" />
+        <CounterInput v-bind="args" size="sm" />
+        <CounterInput v-bind="args" size="xs" />
+      </div>
+    `,
+  }),
+};
+
+export const Disabled: Story = {
+  args: {
+    defaultValue: 0,
+    max: 10,
+    disabled: true,
+  },
+
+  render: args => ({
+    components: { CounterInput },
+    setup() {
+      return { args };
+    },
+    template: `
+      <CounterInput v-bind="args" />
+    `,
+  }),
+};
+
+export const hasError: Story = {
+  args: {
+    defaultValue: 16,
+    max: 20,
+    hasError: true,
+  },
+
+  render: args => ({
+    components: { CounterInput },
+    setup() {
+      return { args };
+    },
+    template: `
+      <CounterInput v-bind="args" />
     `,
   }),
 };
