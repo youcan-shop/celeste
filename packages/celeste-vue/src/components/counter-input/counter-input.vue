cf542042b8712f8eb58f0e33de9e963a380205bb
Badreddine Ibril <148057247+ibrilBadreddine@users.noreply.github.com>
2025-08-27 10:30:09 +0100

    feat: counter input (#64)
    * feat: counter input
    
    * chore: handle input states
    
    * chore: add component stories
    
    * chore: add more stories
    
    * refactor: tiny code improvements
    
    * chore: create input folder group
    
    * refactor: remove placeholder styles
    
    * refactor: no placeholders anymore

---
+++ packages/celeste-vue/src/components/counter-input/counter-input.vue
@@ -0,0 +1,131 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+import { useDelegatedProps } from '@/composables/use-delegated-props';
+import clsx from 'clsx';
+import { NumberFieldDecrement, NumberFieldIncrement, NumberFieldInput, NumberFieldRoot, type NumberFieldRootEmits, type NumberFieldRootProps, useForwardPropsEmits } from 'radix-vue';
+import CompactButton from '../button/compact-button.vue';
+
+const props = withDefaults(
+  defineProps<NumberFieldRootProps & {
+    class?: HTMLAttributes['class'];
+    size?: 'xs' | 'sm' | 'md';
+    hasError?: boolean;
+  }>(),
+  {
+    size: 'md',
+    defaultValue: 0,
+    hasError: false,
+  },
+);
+const emits = defineEmits<NumberFieldRootEmits>();
+
+const delegatedProps = useDelegatedProps(props, 'class');
+const forwarded = useForwardPropsEmits(delegatedProps, emits);
+</script>
+
+<template>
+  <NumberFieldRoot
+    v-bind="forwarded"
+    :has-error="hasError"
+    :class="clsx('celeste-counter-input-wrapper', props.class)"
+  >
+    <NumberFieldDecrement as-child class="celeste-counter-input-decrement">
+      <CompactButton variant="ghost" icon="i-celeste-subtract-line" />
+    </NumberFieldDecrement>
+    <NumberFieldInput class="celeste-counter-input" />
+    <NumberFieldIncrement as-child class="celeste-counter-input-increment">
+      <CompactButton variant="ghost" icon="i-celeste-add-line" />
+    </NumberFieldIncrement>
+  </NumberFieldRoot>
+</template>
+
+<style lang="scss">
+.celeste-counter-input-wrapper {
+  --celeste-counter-input-bg-color: var(--color-bg-white-0);
+  --celeste-counter-input-text-color: var(--color-text-strong-950);
+  --celeste-counter-input-drop-shadow: var(--shadow-regular-xs);
+  --celeste-counter-input-outline-color: transparent;
+  --celeste-counter-input-border-color: var(--color-stroke-soft-200);
+
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: var(--celeste-counter-input-height);
+  padding-inline: var(--celeste-counter-input-gap);
+  transition: all var(--animation-fast) ease-out;
+  border: 1px solid var(--celeste-counter-input-border-color);
+  border-radius: var(--celeste-counter-input-radius);
+  outline: 2px solid var(--celeste-counter-input-outline-color);
+  outline-offset: 2px;
+  background-color: var(--celeste-counter-input-bg-color);
+  box-shadow: var(--celeste-counter-input-drop-shadow);
+  color: var(--celeste-counter-input-text-color);
+  font: var(--paragraph-sm);
+  gap: var(--celeste-counter-input-gap);
+
+  &[size='xs'] {
+    --celeste-counter-input-gap: var(--spacing-4);
+    --celeste-counter-input-height: 30px;
+    --celeste-counter-input-radius: var(--radius-8);
+    --celeste-counter-input-padding: calc(var(--spacing-8) - var(--spacing-2));
+  }
+
+  &[size='sm'] {
+    --celeste-counter-input-gap: var(--spacing-6);
+    --celeste-counter-input-height: 34px;
+    --celeste-counter-input-radius: var(--radius-8);
+    --celeste-counter-input-padding: var(--spacing-8);
+  }
+
+  &[size='md'] {
+    --celeste-counter-input-gap: var(--spacing-8);
+    --celeste-counter-input-height: 38px;
+    --celeste-counter-input-radius: var(--radius-10);
+    --celeste-counter-input-padding: var(--spacing-8);
+  }
+
+  &[has-error='true'] {
+    --celeste-counter-input-border-color: var(--color-state-error-base);
+  }
+
+  & > .celeste-counter-input {
+    flex: 2;
+    height: 100%;
+    border: none;
+    outline: none;
+    background-color: transparent;
+    color: inherit;
+    text-align: center;
+  }
+
+  .celeste-counter-input-decrement:active,
+  .celeste-counter-input-increment:active {
+    background-color: var(--color-bg-weak-50);
+    color: var(--color-icon-strong-950);
+  }
+
+  &:has(.celeste-counter-input:focus) {
+    --celeste-counter-input-outline-color: var(--color-stroke-soft-200);
+    --celeste-counter-input-border-color: var(--color-stroke-strong-950);
+
+    &[has-error='true'] {
+      --celeste-counter-input-border-color: var(--color-state-error-base);
+      --celeste-counter-input-outline-color: var(--color-state-error-lighter);
+    }
+  }
+
+  &:has(.celeste-counter-input:disabled) {
+    --celeste-counter-input-bg-color: var(--color-bg-weak-50);
+    --celeste-counter-input-text-color: var(--color-text-disabled-300);
+    --celeste-counter-input-drop-shadow: none;
+    --celeste-counter-input-border-color: transparent;
+
+    pointer-events: none;
+  }
+
+  &:has(.celeste-counter-input:hover:not(:focus, :disabled)) {
+    --celeste-counter-input-border-color: transparent;
+    --celeste-counter-input-bg-color: var(--color-bg-weak-50);
+  }
+}
+</style>
---
+++ packages/celeste-vue/src/components/counter-input/stories/counter-input.stories.ts
@@ -0,0 +1,149 @@
+import type { Meta, StoryObj } from '@storybook/vue3-vite';
+import CounterInput from '../counter-input.vue';
+
+const meta: Meta<typeof CounterInput> = {
+  title: 'Components/Input/Counter Input',
+  component: CounterInput,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof CounterInput>;
+
+export const Default: Story = {
+  args: {
+    defaultValue: 5,
+    max: 10,
+    min: 0,
+  },
+
+  render: args => ({
+    components: { CounterInput },
+    setup() {
+      return { args };
+    },
+    template: `
+      <CounterInput v-bind="args" />
+    `,
+  }),
+};
+
+export const Size: Story = {
+  args: {
+    defaultValue: 5,
+    max: 10,
+    min: 0,
+  },
+
+  render: args => ({
+    components: { CounterInput },
+    setup() {
+      return { args };
+    },
+    template: `
+      <div style="display: grid; gap: 12px;">
+        <CounterInput v-bind="args" size="md" />
+        <CounterInput v-bind="args" size="sm" />
+        <CounterInput v-bind="args" size="xs" />
+      </div>
+    `,
+  }),
+};
+
+export const Disabled: Story = {
+  args: {
+    defaultValue: 0,
+    max: 10,
+    disabled: true,
+  },
+
+  render: args => ({
+    components: { CounterInput },
+    setup() {
+      return { args };
+    },
+    template: `
+      <CounterInput v-bind="args" />
+    `,
+  }),
+};
+
+export const hasError: Story = {
+  args: {
+    defaultValue: 5,
+    max: 10,
+    hasError: true,
+  },
+
+  render: args => ({
+    components: { CounterInput },
+    setup() {
+      return { args };
+    },
+    template: `
+      <CounterInput v-bind="args" />
+    `,
+  }),
+};
+
+export const Decimal: Story = {
+  args: {
+    defaultValue: 5,
+    formatOptions: {
+      signDisplay: 'negative',
+      minimumFractionDigits: 1,
+    },
+  },
+
+  render: args => ({
+    components: { CounterInput },
+    setup() {
+      return { args };
+    },
+    template: `
+      <CounterInput v-bind="args" />
+    `,
+  }),
+};
+
+export const Percentage: Story = {
+  args: {
+    defaultValue: 0.05,
+    step: 0.01,
+    formatOptions: {
+      style: 'percent',
+    },
+  },
+
+  render: args => ({
+    components: { CounterInput },
+    setup() {
+      return { args };
+    },
+    template: `
+      <CounterInput v-bind="args" />
+    `,
+  }),
+};
+
+export const Currency: Story = {
+  args: {
+    defaultValue: 5,
+    formatOptions: {
+      style: 'currency',
+      currency: 'MAD',
+      currencyDisplay: 'code',
+      currencySign: 'accounting',
+    },
+  },
+
+  render: args => ({
+    components: { CounterInput },
+    setup() {
+      return { args };
+    },
+    template: `
+      <CounterInput v-bind="args" />
+    `,
+  }),
+};
