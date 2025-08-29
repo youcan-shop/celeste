a93fb135927be26bf37c4619dbfa7773aa0e913e
Aymane Dara Hlamnach <dara-@outlook.jp>
2025-08-29 14:24:42 +0100

    feat: radio wrappers (#68)

--- packages/celeste-vue/src/components/radio-group/index.ts
+++ packages/celeste-vue/src/components/radio-group/index.ts
@@ -1,5 +1,11 @@
+export * from './radio-group-card.vue';
+export { default as RadioGroupCard } from './radio-group-card.vue';
+
 export * from './radio-group-item.vue';
 export { default as RadioGroupItem } from './radio-group-item.vue';
+
+export * from './radio-group-label.vue';
+export { default as RadioGroupLabel } from './radio-group-label.vue';
 
 export * from './radio-group.vue';
 export { default as RadioGroup } from './radio-group.vue';
