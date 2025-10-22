c93cd7c912d240c4ceeb198640e0d3c05c8c49a8
Eihab Khan <mastereihab@gmail.com>
2025-10-22 15:51:45 +0100

    feat: add upload file area

--- packages/celeste-vue/src/components/file-upload/file-upload-area.vue
+++ packages/celeste-vue/src/components/file-upload/file-upload-area.vue
@@ -1,14 +1,230 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from '@/components/button/button.vue';
+import { useDropZone } from '@vueuse/core';
+import clsx from 'clsx';
+import { type InputHTMLAttributes, useTemplateRef } from 'vue';
+
+const props = withDefaults(defineProps<FileUploadProps>(), {
+  icon: 'i-celeste-upload-cloud-2-line',
+  label: 'Choose a file or drag & drop it here.',
+  description: 'JPEG, PNG, PDF, and MP4 formats, up to 50 MB.',
+  triggerLabel: 'Browse Files',
+  labelOnDrop: 'Drop file to upload',
+  multiple: false,
+  maxSize: 2000,
+  maxFiles: 5,
+  showPreview: false,
+});
+
+const emit = defineEmits<FileUploadAreaEmit>();
+
+const files = defineModel<File[]>({ default: [] });
+
+const dropZoneRef = useTemplateRef<HTMLElement>('dropZoneRef');
+const { isOverDropZone } = useDropZone(dropZoneRef, { onDrop });
+
+function handleUpload(e: Event) {
+  const input = e.target as HTMLInputElement;
+  const newFiles = [...(input.files ?? [])];
+
+  const filesToProcess = newFiles.slice(0, props.maxFiles);
+  const rejectedFiles = newFiles.slice(props.maxFiles);
+
+  if (rejectedFiles.length > 0) {
+    emit('error', {
+      message: 'Files exceeded upload limit',
+      type: 'count',
+      files: rejectedFiles,
+    });
+  }
+
+  const validFiles = filesToProcess.filter(isValidFile);
+
+  files.value = [...validFiles];
+}
+
+function onDrop(droppedFiles: File[] | null) {
+  if (!droppedFiles?.length)
+    return;
+
+  const filesToProcess = droppedFiles.slice(0, props.maxFiles);
+  const rejectedFiles = droppedFiles.slice(props.maxFiles);
+
+  if (rejectedFiles.length > 0) {
+    emit('error', {
+      message: 'Files exceeded upload limit',
+      type: 'count',
+      files: rejectedFiles,
+    });
+  }
+
+  const validFiles = droppedFiles.filter(isValidFile);
+  files.value = [...validFiles];
+}
+
+function isValidFile(file: File): boolean {
+  const maxBytes = props.maxSize * 1024;
+
+  if (file.size > maxBytes) {
+    emit('error', {
+      message: `File too large: ${file.name}`,
+      type: 'size',
+      files: [file],
+    });
+
+    return false;
+  }
+
+  const acceptableTypes = props.accept?.split(',') ?? [];
+
+  if (acceptableTypes.length > 0 && !acceptableTypes.includes(file.type)) {
+    emit('error', {
+      message: 'Unsupported type',
+      type: 'type',
+      files: [file],
+    });
+
+    return false;
+  }
+
+  return true;
+}
+
+function handleTriggerClick(e: Event) {
+  if (props.onTriggerClick) {
+    e.preventDefault();
+    props.onTriggerClick();
+  }
+}
+</script>
+
+<script lang="ts">
+export interface FileUploadProps {
+  label?: string;
+  description?: string;
+  labelOnDrop?: string;
+  triggerLabel?: string;
+  maxSize?: number;
+  maxFiles?: number;
+  multiple?: boolean;
+  icon?: string;
+  accept?: InputHTMLAttributes['accept'];
+  onTriggerClick?: () => void;
+  class?: InputHTMLAttributes['class'];
+}
+
+export interface FileError {
+  message: string;
+  type: 'size' | 'type' | 'count';
+  files: File[];
+}
+
+export interface FileUploadAreaEmit {
+  error: [error: FileError];
+}
+</script>
 
 <template>
-  <div class="celeste-file-upload">
-    File upload area
-  </div>
+  <label
+    ref="dropZoneRef"
+    :class="clsx(
+      'celeste-file-upload-area',
+      props.class,
+    )"
+    :data-on-drop="isOverDropZone"
+  >
+    <i :class="icon" />
+    <div class="celeste-file-upload-area-content">
+      <span class="celeste-file-upload-area-label">
+        {{ isOverDropZone ? labelOnDrop : label }}
+      </span>
+      <span
+        v-if="!isOverDropZone"
+        class="celeste-file-upload-area-description"
+      >
+        {{ description }}
+      </span>
+    </div>
+    <Button
+      v-if="!isOverDropZone"
+      as-child
+      intent="neutral"
+      variant="stroke"
+      size="xs"
+      class="celeste-file-upload-area-trigger"
+      @click="handleTriggerClick"
+    >
+      <span>{{ triggerLabel }}</span>
+    </Button>
+    <input
+      v-bind="$attrs"
+      type="file"
+      :multiple="multiple"
+      :accept="accept"
+      @change="handleUpload"
+    >
+  </label>
 </template>
 
 <style scoped lang="scss">
-.celeste-file-upload {
-  display: grid;
-  gap: var(--spacing-12);
+.celeste-file-upload-area {
+  display: flex;
+  box-sizing: border-box;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 204px;
+  gap: calc(var(--spacing-10) * 2);
+  padding: calc(var(--spacing-16) * 2);
+  transition: background-color var(--animation-normal) ease-out;
+  border: 1px dashed var(--color-stroke-sub-300);
+  border-radius: var(--radius-12);
+  background-color: var(--color-bg-white-0);
+  cursor: pointer;
+
+  &:hover {
+    background-color: var(--color-bg-weak-50);
+  }
+
+  & > i {
+    width: 24px;
+    height: 24px;
+    color: var(--color-icon-sub-600);
+  }
+
+  .celeste-file-upload-area-content {
+    display: grid;
+    gap: var(--spacing-6);
+    text-align: center;
+
+    .celeste-file-upload-area-label {
+      color: var(--color-text-strong-950);
+      font: var(--label-sm);
+    }
+
+    .celeste-file-upload-area-description {
+      color: var(--color-text-sub-600);
+      font: var(--label-xs);
+    }
+  }
+
+  .celeste-file-upload-area-trigger {
+    background-color: var(--color-bg-white-0) !important;
+
+    &:hover {
+      background-color: transparent !important;
+      box-shadow: none !important;
+    }
+  }
+
+  & > input[type='file'] {
+    position: absolute;
+    opacity: 0;
+    scale: 0;
+  }
+
+  &[data-on-drop='true'] {
+    background-color: var(--color-bg-weak-50);
+  }
 }
 </style>
--- packages/celeste-vue/src/components/file-upload/stories/file-upload-area.stories.ts
+++ packages/celeste-vue/src/components/file-upload/stories/file-upload-area.stories.ts
@@ -1,8 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/vue3-vite';
-import FileUploadArea from '../file-upload-area.vue';
+import { ref, watch } from 'vue';
+import FileUploadArea, { type FileError } from '../file-upload-area.vue';
 
 const meta: Meta<typeof FileUploadArea> = {
-  title: 'Components/File Upload / File Upload Area',
+  title: 'Components/File Upload',
   component: FileUploadArea,
 };
 
@@ -12,15 +13,26 @@ type Story = StoryObj<typeof FileUploadArea>;
 
 export const Default: Story = {
   args: {
+    multiple: true,
   },
 
   render: args => ({
     components: { FileUploadArea },
     setup() {
-      return { args };
+      const files = ref([]);
+
+      watch(files, (newValue) => {
+        // eslint-disable-next-line no-console
+        console.log('v-model changed:', newValue);
+      });
+      function handleError(error: FileError): void {
+        console.error(error);
+      };
+
+      return { args, files, handleError };
     },
     template: `
-      <FileUploadArea v-bind="args" />
+      <FileUploadArea v-bind="args" v-model="files" @error="handleError" />
     `,
   }),
 };
