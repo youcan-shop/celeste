7afb1d1f971a191124dff6a0c385f2685f7e66f6
Eihab Khan <mastereihab@gmail.com>
2025-10-22 16:18:26 +0100

    chore: improve story

--- packages/celeste-vue/src/components/file-upload/stories/file-upload-area.stories.ts
+++ packages/celeste-vue/src/components/file-upload/stories/file-upload-area.stories.ts
@@ -1,6 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/vue3-vite';
-import { ref } from 'vue';
+import Sonner from '@/components/sonner/sonner.vue';
+import { toast } from '@/utils/sonner';
+import { ref, watch } from 'vue';
 import FileUploadArea, { type FileError } from '../file-upload-area.vue';
+import 'vue-sonner/style.css';
 
 const meta: Meta<typeof FileUploadArea> = {
   title: 'Components/File Upload',
@@ -21,6 +24,11 @@ export const Default: Story = {
     setup() {
       const files = ref([]);
 
+      watch(files, (newValue) => {
+        // eslint-disable-next-line no-console
+        console.log('v-model changed:', newValue);
+      });
+
       function handleError(error: FileError): void {
         console.error(error);
       };
@@ -29,6 +37,40 @@ export const Default: Story = {
     },
     template: `
       <FileUploadArea v-bind="args" v-model="files" @error="handleError" />
+    `,
+  }),
+};
+
+export const CustomTriggerHandler: Story = {
+  args: {
+    multiple: true,
+  },
+
+  render: args => ({
+    components: { FileUploadArea, Sonner },
+    setup() {
+      const files = ref([]);
+
+      watch(files, (newValue) => {
+        // eslint-disable-next-line no-console
+        console.log('v-model changed:', newValue);
+      });
+
+      function handleTriggerClick(): void {
+        toast('Upload trigger clicked!');
+      };
+
+      function handleError(error: FileError): void {
+        console.error(error);
+      };
+
+      return { args, files, handleError, handleTriggerClick };
+    },
+    template: `
+    <div class="sonner-story-container">
+      <Sonner position="top-center" />
+      <FileUploadArea v-bind="args" v-model="files" @error="handleError" @triggerClick="handleTriggerClick" />
+    </div>
     `,
   }),
 };
