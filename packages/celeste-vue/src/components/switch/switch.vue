934ecef049c34f12d81840f02b3c5910b3918f53
Eihab Khan <143792300+eihabkhan@users.noreply.github.com>
2025-11-01 11:20:30 +0100

    fix: switch thumb size (#78)
    * fix: switch thumb size
    
    * chore: code cleanup
    
    * chore: fix linting issues
    
    * chore: fix thumb border width
    
    * chore: improve thumb transition

--- packages/celeste-vue/src/components/switch/switch.vue
+++ packages/celeste-vue/src/components/switch/switch.vue
@@ -22,47 +22,65 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits);
 
 <style lang="scss">
 .celeste-switch {
-  --switch-w: 26px;
-  --switch-h: 16px;
+  --width: 26px;
+  --height: 16px;
+  --bg-color: var(--color-bg-soft-200);
   --animation-function: cubic-bezier(0.18, 0.89, 0.43, 1.19);
 
   position: relative;
-  width: var(--switch-w);
-  height: var(--switch-h);
+  width: var(--width);
+  height: var(--height);
   transition: background var(--animation-fast) ease-out;
   border: 1px solid transparent;
   border-radius: var(--radius-full);
-  background-color: var(--color-bg-soft-200);
+  background-color: var(--bg-color);
   cursor: pointer;
 
   .celeste-switch-thumb {
-    --switch-thumb-size: 12px;
+    --thumb-size: 12px;
+    --thumb-hole: 4px;
 
     position: absolute;
-    width: calc(var(--switch-thumb-size) / 3);
-    height: calc(var(--switch-thumb-size) / 3);
+    box-sizing: border-box;
+    width: var(--thumb-size);
+    height: var(--thumb-size);
     transform: translateY(-50%);
     transition-property: inset-inline-start, width, height;
     transition-duration: var(--animation-fast);
     transition-timing-function: var(--animation-function), ease-out, ease-out;
-    border: calc(var(--switch-thumb-size) / 4) solid var(--color-static-white);
     border-radius: var(--radius-full);
+    background-color: var(--color-static-white);
     box-shadow: var(--shadow-toggle-switch);
     inset-block-start: 50%;
     inset-inline-start: 1px;
+    will-change: transform;
 
     &[data-state='checked'] {
-      inset-inline-start: calc(100% - (var(--switch-thumb-size) - 1px));
+      inset-inline-start: calc(100% - var(--thumb-size) - 1px);
+    }
+
+    &::before {
+      content: '';
+      position: absolute;
+      width: var(--thumb-hole);
+      height: var(--thumb-hole);
+      margin: auto;
+      transition: background var(--animation-fast) ease-out;
+      border-radius: var(--radius-full);
+      background-color: var(--bg-color);
+      inset: var(--spacing-0);
     }
   }
 
   &[aria-checked='true'] {
-    background-color: var(--color-primary-base);
+    --bg-color: var(--color-primary-base);
   }
 
   &:disabled {
+    --bg-color: var(--color-bg-white-0);
+
     border-color: var(--color-stroke-soft-200);
-    background-color: var(--color-bg-white-0);
+    background-color: var(--bg-color);
     pointer-events: none;
 
     .celeste-switch-thumb {
@@ -72,20 +90,20 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits);
       box-shadow: none;
 
       &[data-state='checked'] {
-        inset-inline-start: calc(100% - var(--switch-thumb-size));
+        inset-inline-start: calc(100% - var(--thumb-size));
       }
     }
   }
 
   &:active:not(:disabled) .celeste-switch-thumb {
-    --switch-thumb-size: 10px;
+    --thumb-size: 10px;
   }
 
   &:hover:not(:active, :disabled) {
-    background-color: var(--color-bg-sub-300);
+    --bg-color: var(--color-bg-sub-300);
 
     &[aria-checked='true'] {
-      background-color: var(--color-primary-darker);
+      --bg-color: var(--color-primary-darker);
     }
   }
 }
