c92c8a261946fd79886a84562dd935e4167f7751
Eihab Khan <143792300+eihabkhan@users.noreply.github.com>
2025-12-09 09:42:58 +0100

    style: scroll area style improvements (#84)

--- packages/celeste-vue/src/components/scroll-area/scroll-bar.vue
+++ packages/celeste-vue/src/components/scroll-area/scroll-bar.vue
@@ -1,36 +1,52 @@
 <script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+import { useDelegatedProps } from '@/composables';
 import clsx from 'clsx';
 import { ScrollAreaScrollbar, type ScrollAreaScrollbarProps, ScrollAreaThumb } from 'radix-vue';
-import { computed, type HTMLAttributes } from 'vue';
 
 const props = withDefaults(defineProps<ScrollAreaScrollbarProps & { class?: HTMLAttributes['class'] }>(), {
   orientation: 'vertical',
 });
 
-const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props;
-
-  return delegated;
-});
+const delegatedProps = useDelegatedProps(props, 'class');
 </script>
 
 <template>
   <ScrollAreaScrollbar
     v-bind="delegatedProps"
-    :class="
-      clsx('flex touch-none select-none transition-colors',
-           orientation === 'vertical'
-             && 'h-full w-1.5 border-l border-l-transparent p-px',
-           orientation === 'horizontal'
-             && 'h-1.5 flex-col border-t border-t-transparent p-px',
-           props.class)"
+    :class="clsx('celeste-scroll-bar', props.class)"
   >
-    <ScrollAreaThumb class="thumb relative flex-1 rounded-full" />
+    <ScrollAreaThumb class="celeste-scroll-bar-thumb" />
   </ScrollAreaScrollbar>
 </template>
 
-<style scoped>
-.thumb {
-  background-color: var(--color-bg-soft-200);
+<style scoped lang="scss">
+.celeste-scroll-bar {
+  --bar-size: 8px;
+
+  display: flex;
+  user-select: none;
+  touch-action: none;
+
+  &[data-orientation='vertical'] {
+    width: var(--bar-size);
+    height: 100%;
+    padding: var(--spacing-2);
+    border-inline-start: 1px solid transparent;
+  }
+
+  &[data-orientation='horizontal'] {
+    flex-direction: column;
+    height: var(--bar-size);
+    padding: var(--spacing-2);
+    border-block-start: 1px solid transparent;
+  }
+}
+
+.celeste-scroll-bar-thumb {
+  position: relative;
+  flex: 1;
+  border-radius: calc(infinity * 1px);
+  background-color: var(--color-bg-sub-300);
 }
 </style>
