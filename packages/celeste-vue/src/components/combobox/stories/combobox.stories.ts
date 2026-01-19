9cc28594337fca1425cdd86dadd2a86e6d17e4c9
Eihab Khan <mastereihab@gmail.com>
2026-01-19 10:26:25 +0100

    chore: combobox story cleanup

--- packages/celeste-vue/src/components/combobox/stories/combobox.stories.ts
+++ packages/celeste-vue/src/components/combobox/stories/combobox.stories.ts
@@ -12,7 +12,7 @@ export default meta;
 type Story = StoryObj<typeof Combobox>;
 
 const OPTIONS = [
-  { value: { id: 1 }, label: 'A really long product title, now why would anyone have a title this long, i don\'t know really, but sellers do have em and it breaks the theme editor. So yeah, we gotta make sure this works', sublabel: 'Teacher' },
+  { value: { id: 1 }, label: 'Durward Reynolds ', sublabel: 'Teacher' },
   { value: { id: 2 }, label: 'Kenton Towne', icon: 'i-celeste-globe-line' },
   { value: { id: 3 }, label: 'Therese Wunsch', sublabel: 'Engineer' },
   { value: { id: 4 }, label: 'Benedict Kessler', image: 'https://cdn3.pixelcut.app/7/20/uncrop_hero_bdf08a8ca6.jpg' },
