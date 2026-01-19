9200109ea40cd3b2bcb684cc213b9a1899012b17
Eihab Khan <mastereihab@gmail.com>
2026-01-19 10:27:44 +0100

    chore: fix typo in story

--- packages/celeste-vue/src/components/combobox/stories/combobox.stories.ts
+++ packages/celeste-vue/src/components/combobox/stories/combobox.stories.ts
@@ -12,7 +12,7 @@ export default meta;
 type Story = StoryObj<typeof Combobox>;
 
 const OPTIONS = [
-  { value: { id: 1 }, label: 'Durward Reynolds ', sublabel: 'Teacher' },
+  { value: { id: 1 }, label: 'Durward Reynolds', sublabel: 'Teacher' },
   { value: { id: 2 }, label: 'Kenton Towne', icon: 'i-celeste-globe-line' },
   { value: { id: 3 }, label: 'Therese Wunsch', sublabel: 'Engineer' },
   { value: { id: 4 }, label: 'Benedict Kessler', image: 'https://cdn3.pixelcut.app/7/20/uncrop_hero_bdf08a8ca6.jpg' },
