4f8f577f91816a0098c4272acaf7ed1b26cbcaab
Aymane D. Hlamnach <dara-@outlook.jp>
2026-09-04 17:33:24 +0100

    feat(segmented-control): sliding indicator

--- packages/celeste-vue/src/components/segmented-control/segmented-control.vue
+++ packages/celeste-vue/src/components/segmented-control/segmented-control.vue
@@ -4,28 +4,43 @@ import type { HTMLAttributes } from 'vue';
 import clsx from 'clsx';
 import { ToggleGroupRoot as SegmentedControlRoot, useForwardPropsEmits } from 'reka-ui';
 import { useDelegatedProps } from '@/composables/use-delegated-props';
+import { useTabObserver } from '@/composables/use-tab-observer';
 
 const props = defineProps<Omit<ToggleGroupRootProps, 'type'> & { class?: HTMLAttributes['class'] }>();
 const emits = defineEmits<ToggleGroupRootEmits>();
 
 const delegatedProps = useDelegatedProps(props, 'class');
 const forwarded = useForwardPropsEmits(delegatedProps, emits);
+
+const { listRef, indicator, ready } = useTabObserver('[data-state="on"]');
 </script>
 
 <template>
   <SegmentedControlRoot
+    ref="listRef"
     :class="clsx('celeste-segmented-control', props.class)"
     v-bind="forwarded"
     type="single"
   >
     <slot />
+    <div
+      class="celeste-segmented-control-indicator"
+      :data-ready="ready"
+      :style="{
+        width: `${indicator.width}px`,
+        height: `${indicator.height}px`,
+        transform: `translate3d(${indicator.left}px, ${indicator.top}px, 0)`,
+      }"
+    />
   </SegmentedControlRoot>
 </template>
 
 <style scoped>
 .celeste-segmented-control {
   display: flex;
+  position: relative;
   gap: var(--spacing-4);
+  isolation: isolate;
   align-items: center;
   width: 100%;
   padding: var(--spacing-4);
@@ -34,6 +49,26 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits);
 
   &[data-orientation='vertical'] {
     flex-direction: column;
+  }
+}
+
+.celeste-segmented-control-indicator {
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  transition-property: transform, width, height;
+  transition-duration: 0s;
+  transition-timing-function: cubic-bezier(0.65, 0, 0.35, 1);
+  border-radius: var(--radius-6);
+  opacity: 0;
+  background-color: var(--color-bg-white-0);
+  box-shadow: var(--shadow-toggle-switch);
+  pointer-events: none;
+
+  &[data-ready='true'] {
+    transition-duration: var(--animation-normal);
+    opacity: 1;
   }
 }
 </style>
